9c39de83cc008183a1db1f7e95bfb78d0437b4eb
Mando1mS <94066867+Mando1mS@users.noreply.github.com>
2023-10-19 19:11:31 +0100

    algumas pequenas mudanças

--- code/src/application_layer.c
+++ code/src/application_layer.c
@@ -5,43 +5,62 @@
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    unsigned char *source=NULL;
-    FILE *fp=fopen("penguin.gif","r");
-    if(fp!=NULL)
-    {
-        if(fseek(fp,0L,SEEK_END)==FALSE)
-        {
-            int buffersize=ftell(fp);
-            if(buffersize==-1)
-                ferror(fp);
-            source=malloc(sizeof(unsigned char)*(buffersize+1));
-            if(fseek(fp,0L,SEEK_SET)!=0)
-                ferror(fp);
-            size_t newLength = fread(source,sizeof(unsigned char),buffersize,fp);
-            if(ferror(fp)!=0)
-            {
-                fputs("Error handling the buffer!",stderr);
-            }
-            else{ source[newLength+1]='\0';}
-        }
-        fclose(fp);
-    }
-    int i=0;
-    while(source[i]!='\0')
-    {
-        printf("Byte: %d \n",source[i]);
-        i++;
-    }
-    free(source);
+    //Comum para ambos.
     LinkLayer connectionParameters;
     strcpy(connectionParameters.serialPort,serialPort);
     connectionParameters.role= strcmp(role,"rx") ? LlTx : LlRx;
     connectionParameters.baudRate = baudRate;
     connectionParameters.nRetransmissions = nTries;
     connectionParameters.timeout = timeout;
+    int pass_open = llopen(connectionParameters);
+    if(pass_open==TRUE)
+    {
+        if(connectionParameters.role==LlTx)
+            {
+                printf("Executando o llwrite()\n");
+                unsigned char *source=NULL;
+                int buffersize=0;
+                FILE *fp=fopen(filename,"r");
+                if(fp!=NULL)
+                    {   
+                        if(fseek(fp,0L,SEEK_END)==FALSE)
+                            {
+                                buffersize=ftell(fp);
+                                if(buffersize==-1)
+                                    ferror(fp);
+                                source=malloc(sizeof(unsigned char)*(buffersize+1));
+                                if(fseek(fp,0L,SEEK_SET)!=0)
+                                    ferror(fp);
+                                size_t newLength = fread(source,sizeof(unsigned char),buffersize,fp);
+                                if(ferror(fp)!=0)
+                                {
+                                    fputs("Error handling the buffer!",stderr);
+                                }
+                            else{ source[newLength+1]='\0';}
+                            }
+                    fclose(fp);
+                    }
+                int i=0;
+                while(source[i]!='\0')
+                {
+                    printf("Byte: %d \n",source[i]);
+                    i++;
+                 }
+            llwrite(source,buffersize);
+            free(source);
+        }
+        if(connectionParameters.role==LlRx)
+        {
+            printf("Executando o llread()\n");
+        }
+    }
+    else{
+        printf("Error found while opening the port connection\n");
+    }
     
     
-    llopen(connectionParameters);
+    
+    
     //llwrite(Buffer,buffersize);
     
 }
